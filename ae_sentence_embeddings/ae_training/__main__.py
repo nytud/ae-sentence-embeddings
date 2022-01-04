07f353fe28eb1bb2e4019b709aeae14234c36f18
nyekibence <nyeki.bence96@gmail.com>
2022-01-04 11:16:37 +0100

    Training without hyperparameter tuning added

--- ae_sentence_embeddings/ae_training/__main__.py
+++ ae_sentence_embeddings/ae_training/__main__.py
@@ -1,9 +1,15 @@
 """Tune hyperparameters or run full pre-ae_training"""
 
 from argparse import Namespace
+import warnings
 
 from ae_sentence_embeddings.modeling_tools import get_training_args, read_json
 from ae_sentence_embeddings.ae_training.hyperparameter_tuning import search_hparams
+from ae_sentence_embeddings.ae_training.pretrain import (
+    pretrain_transformer_ae,
+    group_arguments,
+    get_transformer_configs
+)
 
 
 def get_args() -> Namespace:
@@ -19,9 +25,23 @@ def main() -> None:
     args = get_args()
     arg_dict = read_json(args.config_file)
     if args.search_hparams:
+        # This is still experimental and is to be fixed!
+        warnings.warn("Hyperparameter tuning is still experimental and does not work properly")
         search_hparams(arg_dict)
     else:
-        raise NotImplementedError
+        dataset_split_paths, data_args, lr_args, adamw_args, save_log_args = group_arguments(arg_dict)
+        transformer_configs = get_transformer_configs(arg_dict)
+        pretrain_transformer_ae(
+            dataset_split_paths=dataset_split_paths,
+            data_args=data_args,
+            lr_args=lr_args,
+            adamw_args=adamw_args,
+            save_log_args=save_log_args,
+            transformer_configs=transformer_configs,
+            num_epochs=arg_dict["num_epochs"],
+            dataset_cache_dir=arg_dict["hgf_cache_dir"],
+            devices=arg_dict["devices"]
+        )
 
 
 if __name__ == '__main__':
