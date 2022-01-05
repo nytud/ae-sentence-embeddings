ddf1b78dd32fb08dbb06a42c5cb85a1008616e9f
nyekibence <nyeki.bence96@gmail.com>
2022-01-05 13:42:49 +0100

    Tokenization fix: max length set

--- ae_sentence_embeddings/data/__main__.py
+++ ae_sentence_embeddings/data/__main__.py
@@ -15,6 +15,8 @@ def get_tokenization_args() -> Namespace:
     parser.add_argument("tokenizer_output_path", help="Path to the output `.jsonl` dataset")
     parser.add_argument("--tokenizer", default="SZTAKI-HLT/hubert-base-cc",
                         help="Tokenizer name or path. Defaults to `SZTAKI-HLT/hubert-base-cc`")
+    parser.add_argument("--max-length", dest="max_length", type=int, default=128,
+                        help="Maximal sequence length in the number of tokens. Defaults to 128")
     parser.add_argument("--cache-dir", dest="cache_dir", help="Optional. The cache directory for the dataset")
     return parser.parse_args()
 
@@ -24,7 +26,7 @@ def main() -> None:
     args = vars(get_tokenization_args())
     raw_dataset = load_dataset('text', data_files=[args["text_dataset_path"]], split='train',
                                cache_dir=args["cache_dir"])
-    tokenizer = AutoTokenizer.from_pretrained(args["tokenizer"])
+    tokenizer = AutoTokenizer.from_pretrained(args["tokenizer"], model_max_length=args["max_length"])
     tokenized_dataset = tokenize_hgf_dataset(dataset=raw_dataset, tokenizer=tokenizer)
     tokenized_dataset.to_json(args["tokenizer_output_path"], force_ascii=False)
 
