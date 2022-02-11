487a466d6823654dcc607b28b8109b6aed6fd56e
nyekibence <nyeki.bence96@gmail.com>
2022-02-11 12:39:52 +0100

    feature swapping option added to dataset tokenization

--- test/data/test_tokenize_dataset.py
+++ test/data/test_tokenize_dataset.py
@@ -13,7 +13,7 @@ from ae_sentence_embeddings.data import tokenize_hgf_dataset
 class DataTest(tf.test.TestCase):
 
     def setUp(self) -> None:
-        """Fixture setup. This creates a monolingual data dictionary and a tokenizer"""
+        """Fixture setup. This creates monolingual and bilingual data dictionaries and a tokenizer"""
         super().setUp()
         self.data_mono = {
             "id": list(range(1, 5)),
@@ -24,37 +24,7 @@ class DataTest(tf.test.TestCase):
                 "And here is the last attempt to make some dummy text!"
             ]
         }
-        self.bert_tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
-
-    @staticmethod
-    def _prepare_for_test(data_dict: Dict[str, Any], tokenizer: Union[BertTokenizer, Tokenizer],
-                          text_col_names: Iterable[str]) -> HgfDataset:
-        """Helper function for tokenization testing
-
-        Args:
-            data_dict: The input dataset as a dictionary that can be converted to a `datasets.Dataset`
-            text_col_names: Names of the text columns
-
-        Returns:
-            The tokenized dataset
-        """
-        dataset = HgfDataset.from_dict(data_dict)
-        dataset = tokenize_hgf_dataset(
-            dataset, tokenizer, text_col_names=text_col_names, remove_old_cols=True)
-        print(f"An example from the tokenized dataset is:\n{dataset[0]}")
-        return dataset
-
-    def test_tokenize_hgf_mono(self) -> None:
-        """Test the tokenization of a monolingual `datasets.Dataset`"""
-        text_col_names = ("text",)
-        dataset = self._prepare_for_test(self.data_mono, self.bert_tokenizer, text_col_names)
-        self.assertIsInstance(dataset[1]["input_ids"], list)
-        self.assertIsNotNone(dataset[1].get("attention_mask"))
-        self.assertEqual(len(self.data_mono["text"]), len(dataset))
-
-    def test_tokenize_hgf_bi(self) -> None:
-        """Test the tokenization of a bilingual `datasets.Dataset`"""
-        data_bi = {
+        self.data_bi = {
             "id": self.data_mono["id"],
             "text_en": self.data_mono["text"],
             "text_hu": [
@@ -64,11 +34,64 @@ class DataTest(tf.test.TestCase):
                 "És itt az utolsó kísérlet próbaszöveg alkotására!"
             ]
         }
+        self.bert_tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
+
+    @staticmethod
+    def _prepare_for_test(data_dict: Dict[str, Any], tokenizer: Union[BertTokenizer, Tokenizer],
+                          text_col_names: Iterable[str], swap_feature_cols: bool = False) -> HgfDataset:
+        """Helper function for tokenization testing
+
+        Args:
+            data_dict: The input dataset as a dictionary that can be converted to a `datasets.Dataset`
+            text_col_names: Names of the text columns
+            swap_feature_cols: Specify whether feature columns should be swapped between languages.
+                Defaults to `False`
+
+        Returns:
+            The tokenized dataset
+        """
+        dataset = HgfDataset.from_dict(data_dict)
+        dataset = tokenize_hgf_dataset(
+            dataset, tokenizer,
+            text_col_names=text_col_names,
+            remove_old_cols=True,
+            swap_feature_cols=swap_feature_cols
+        )
+        return dataset
+
+    def test_tokenize_hgf_mono(self) -> None:
+        """Test the tokenization of a monolingual `datasets.Dataset`"""
+        text_col_names = ("text",)
+        dataset = self._prepare_for_test(self.data_mono, self.bert_tokenizer, text_col_names)
+        print(f"An example from the tokenized monolingual dataset is:\n{dataset[0]}")
+        self.assertIsInstance(dataset[1]["input_ids"], list)
+        self.assertIsNotNone(dataset[1].get("attention_mask"))
+        self.assertEqual(len(self.data_mono["text"]), len(dataset))
+
+    def test_tokenize_hgf_bi(self) -> None:
+        """Test the tokenization of a bilingual `datasets.Dataset`"""
         text_col_names = ("text_en", "text_hu")
-        dataset = self._prepare_for_test(data_bi, self.bert_tokenizer, text_col_names)
+        dataset = self._prepare_for_test(self.data_bi, self.bert_tokenizer, text_col_names)
+        print(f"An example from the tokenized bilingual dataset is:\n{dataset[0]}")
         self.assertIsInstance(dataset[1]["input_ids_en"], list)
         self.assertIsNotNone(dataset[1].get("attention_mask_en"))
-        self.assertEqual(len(data_bi["text_en"]), len(dataset))
+        self.assertEqual(len(self.data_bi["text_en"]), len(dataset))
+
+    def test_swapped_bi_tokenization(self) -> None:
+        """Test if swapping target columns works as expected"""
+        text_col_names = ("text_en", "text_hu")
+        translation_data_point = self._prepare_for_test(
+            self.data_bi, self.bert_tokenizer, text_col_names, swap_feature_cols=True)[0]
+        print(f"An example from the tokenized bilingual translation dataset is:\n{translation_data_point}")
+        auto_encoding_data_point = self._prepare_for_test(
+            self.data_bi, self.bert_tokenizer, text_col_names)[0]
+        print(f"An example from the tokenized bilingual "
+              f"auto-encoding dataset is:\n{auto_encoding_data_point}")
+        self.assertAllEqual(auto_encoding_data_point["target_en"], translation_data_point["target_en"])
+        self.assertAllEqual(auto_encoding_data_point["target_hu"], translation_data_point["target_hu"])
+        self.assertAllEqual(auto_encoding_data_point["input_ids_en"], translation_data_point["input_ids_hu"])
+        self.assertAllEqual(
+            auto_encoding_data_point["attention_mask_en"], translation_data_point["attention_mask_hu"])
 
 
 if __name__ == '__main__':
