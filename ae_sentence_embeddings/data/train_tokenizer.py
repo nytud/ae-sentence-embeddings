c1383cdeb63e6c37304bc3c4b1fded01202a1f4f
nyekibence <nyeki.bence96@gmail.com>
2022-02-11 15:29:44 +0100

    tokenizer training main function added

--- ae_sentence_embeddings/data/train_tokenizer.py
+++ ae_sentence_embeddings/data/train_tokenizer.py
@@ -1,11 +1,29 @@
-"""Train a BPE tokenizer with the `tokenizers` library"""
+"""A script for training a BPE tokenizer with the `tokenizers` library"""
 
 from typing import Iterable, Optional
+from argparse import Namespace, ArgumentParser
 
 from tokenizers import Tokenizer
 from tokenizers.models import BPE
 from tokenizers.trainers import BpeTrainer
 from tokenizers.pre_tokenizers import Whitespace
+
+
+def get_tokenizer_training_args() -> Namespace:
+    """Get command line arguments for tokenizer training"""
+    parser = ArgumentParser(description="Arguments for training a BPE tokenizer from scratch")
+    parser.add_argument("training_file", help="Path to train plain text training corpus")
+    parser.add_argument("save_path", help="Path where the trained tokenizer will be saved")
+    parser.add_argument("--vocab-size", dest="vocab_size", type=int, default=30000,
+                        help="Required vocabulary size. Defaults to 30000")
+    parser.add_argument("--max-length", dest="max_length", type=int,
+                        help="Optional. Maximal sequence length to which longer sequences will be truncated. "
+                             "If not specified, truncation will not be used")
+    parser.add_argument("--unk-token", dest="unk_token", default="[UNK]",
+                        help="Unknown token. Defaults to `'[UNK]'`")
+    parser.add_argument("--special-tokens", nargs='*', dest="special_tokens",
+                        help="Optional. A list of special tokens")
+    return parser.parse_args()
 
 
 def train_bpe(
@@ -36,3 +54,21 @@ def train_bpe(
     tokenizer.pre_tokenizer = Whitespace()
     tokenizer.train(files, trainer)
     return tokenizer
+
+
+def train_tokenizer_main() -> None:
+    """Main function for tokenizer training"""
+    args = get_tokenizer_training_args()
+    tokenizer = train_bpe(
+        files=[args.training_file],
+        vocab_size=args.vocab_size,
+        special_tokens=args.special_tokens,
+        unk_token=args.unk_token
+    )
+    if args.max_length is not None:
+        tokenizer.enable_truncation(max_length=args.max_length)
+    tokenizer.save(args.save_path)
+
+
+if __name__ == '__main__':
+    train_tokenizer_main()
