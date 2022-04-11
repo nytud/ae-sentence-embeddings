99461fca5740d53a76369b8c2f6c9a2ea1d17f3d
nyekibence <nyeki.bence96@gmail.com>
2022-04-11 13:11:33 +0200

    fine-tuning config example added

--- ae_sentence_embeddings/fine_tuning/__main__.py
+++ ae_sentence_embeddings/fine_tuning/__main__.py
@@ -8,15 +8,18 @@ from ae_sentence_embeddings.ae_training.pretrain import (
     collect_wandb_args,
     group_train_args_from_flat
 )
+from ae_sentence_embeddings.argument_handling import check_if_positive_int, check_if_dir
 
 
 def main() -> None:
     """Main function"""
     config = collect_wandb_args()
     train_args = group_train_args_from_flat(config)
+    model_ckpt = check_if_dir(config["model_ckpt"])
+    num_labels = check_if_positive_int(config["num_labels"])
     fine_tune(
-        model_ckpt=config["model_ckpt"],
-        num_labels=config["num_labels"],
+        model_ckpt=model_ckpt,
+        num_labels=num_labels,
         dataset_split_paths=train_args.data_split_path_args,
         data_stream_args=train_args.data_stream_args,
         adamw_args=train_args.adamw_args,
@@ -29,3 +32,7 @@ def main() -> None:
         use_mcc=bool(config.get("use_mcc")),
         num_epochs=config.get("num_epochs", 2)
     )
+
+
+if __name__ == "__main__":
+    main()
