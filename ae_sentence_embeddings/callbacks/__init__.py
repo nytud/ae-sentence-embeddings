9c3680bd3c97e7289255ee07797427182faaf5ca
nyekibence <nyeki.bence96@gmail.com>
2022-06-27 14:15:25 +0200

    add callback to log the regularization loss average values

--- ae_sentence_embeddings/callbacks/__init__.py
+++ ae_sentence_embeddings/callbacks/__init__.py
@@ -1,2 +1,2 @@
-from .checkpoint_and_log import basic_checkpoint_and_log, DevEvaluator, AeCustomCheckpoint
+from .checkpoint_and_log import basic_checkpoint_and_log, DevEvaluator, AeCustomCheckpoint, RegCallback
 from .schedulers import OneCycleScheduler, CyclicScheduler, KLAnnealer, VaeLogger
--- ae_sentence_embeddings/pretrain_vae.py
+++ ae_sentence_embeddings/pretrain_vae.py
@@ -34,7 +34,7 @@ from ae_sentence_embeddings.argument_handling import (
     RnnArgs
 )
 from ae_sentence_embeddings.modeling_tools import get_custom_logger, read_json, timing
-from ae_sentence_embeddings.callbacks import AeCustomCheckpoint, DevEvaluator, VaeLogger
+from ae_sentence_embeddings.callbacks import AeCustomCheckpoint, DevEvaluator, VaeLogger, RegCallback
 from ae_sentence_embeddings.losses_and_metrics import IgnorantSparseCatCrossentropy
 from ae_sentence_embeddings.models import BertRnnVae
 
@@ -204,6 +204,7 @@ def main() -> None:
             beta_warmup_start=args["beta_warmup_start"],
         ),
         DevEvaluator(devel_dataset, logger="WandB", log_freq=args["validation_freq"]),
+        RegCallback("reg_loss", log_update_freq=args["log_update_freq"]),
         WandbCallback(save_model=False, log_batch_frequency=args["log_update_freq"])
     ]
 
