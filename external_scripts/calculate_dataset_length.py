fdd3747a72b416680b0ff8a75da83de82f971fa9
nyekibence <nyeki.bence96@gmail.com>
2022-07-15 14:14:22 +0200

    dataset length calculation change

--- external_scripts/calculate_dataset_length.py
+++ external_scripts/calculate_dataset_length.py
@@ -8,64 +8,59 @@ calculating the number of batches is not trivial.
 
 from argparse import ArgumentParser, Namespace
 
-from datasets import load_dataset
-
-from ae_sentence_embeddings.argument_handling import (
-    DataStreamArgs,
-    check_if_positive_int,
-    check_if_file,
-    check_if_output_path
-)
-from ae_sentence_embeddings.data import pad_and_batch, convert_to_tf_dataset
+from ae_sentence_embeddings.argument_handling import check_if_positive_int
+from ae_sentence_embeddings.data import load_tokenizer, load_hgf_dataset, tokenize_hgf_dataset
 
 
 def get_data_args() -> Namespace:
     """Get command line arguments"""
     parser = ArgumentParser(description="Command line arguments for batching")
-    parser.add_argument("input_path", type=check_if_file, help="Path to the input `jsonlines` dataset file.")
+    parser.add_argument("dataset", type=load_hgf_dataset,
+                        help="Path to the input `jsonlines` dataset file.")
+    parser.add_argument("--tokenizer", required=True, type=load_tokenizer,
+                        help="Tokenizer name or path.")
     parser.add_argument("--batch-size", dest="batch_size", type=check_if_positive_int, default=32,
                         help="Batch size of the largest bucket. Defaults to `32`.")
-    parser.add_argument("--num-buckets", dest="num_buckets", type=check_if_positive_int, default=2,
-                        help="Number of buckets. Defaults to `2`.")
+    parser.add_argument("--num-buckets", dest="num_buckets", type=check_if_positive_int, default=1,
+                        help="Number of buckets. Defaults to `1`.")
     parser.add_argument("--first-bucket-boundary", dest="first_bucket_boundary", type=check_if_positive_int,
                         default=33, help="Non-inclusive upper boundary of sequence length in the first "
                                          "bucket. Defaults to `33`.")
-    parser.add_argument("--input-padding", dest="input_padding", required=True,
-                        nargs="+", help="Input padding tokens, one per input feature.")
-    parser.add_argument("--target-padding", dest="target_padding", required=True,
-                        nargs="+", help="Target padding tokens, one per target feature.")
-    parser.add_argument("--cache-dir", dest="cache_dir", type=check_if_output_path,
-                        help="Optional. Path to the dataset cache directory.")
-    parser.add_argument("--keep-remainder", dest="drop_remainder", action="store_false",
-                        help="Specify if you want to keep the last batch even if "
-                             "it is smaller than expected.")
+    parser.add_argument("--log-freq", dest="log_freq", type=check_if_positive_int, default=1000,
+                        help="Specify how often to report the number of batches (in number of iterations). "
+                             "Defaults to `1000`.")
     return parser.parse_args()
 
 
 def main() -> None:
     """Main function. Iterate over the dataset to get its length."""
     args = get_data_args()
-    input_padding = [int(pad) for pad in args.input_padding]
-    target_padding = [int(pad) for pad in args.target_padding]
-    if len(input_padding) == 1:
-        input_padding = input_padding[0]
-    if len(target_padding) == 1:
-        target_padding = target_padding[0]
-    data_stream_args = DataStreamArgs(
-        input_padding=input_padding,
-        target_padding=target_padding,
-        batch_size=args.batch_size,
-        num_buckets=args.num_buckets,
-        first_bucket_boundary=args.first_bucket_boundary
-    )
-    dataset = convert_to_tf_dataset(
-        load_dataset("json", data_files=args.input_path, split="train", cache_dir=args.cache_dir))
-    dataset = pad_and_batch(dataset, data_stream_args=data_stream_args,
-                            drop_remainder=args.drop_remainder)
-    i = 0
-    for _ in dataset:
-        i += 1
-    print(f"Length of the batched dataset: {i}")
+    dataset = tokenize_hgf_dataset(
+        args.dataset, tokenizer=args.tokenizer, remove_old_cols=True)
+    boundaries = [(args.first_bucket_boundary - 1) * 2**n + 1 for n in range(args.num_buckets)]
+    del boundaries[-1]
+    sizes = [int(args.batch_size / 2**n) for n in range(args.num_buckets)]
+    buckets = [0] * args.num_buckets
+    log_freq = args.log_freq
+    num_batches, i = 0, 0
+
+    for i, example in enumerate(dataset, start=1):
+        input_len = len(example["input_ids"])
+        for j, boundary in enumerate(boundaries):
+            if input_len < boundary:
+                actual_bucket_id = j
+                break
+        else:
+            actual_bucket_id = len(boundaries)
+        buckets[actual_bucket_id] += 1
+        if buckets[actual_bucket_id] == sizes[actual_bucket_id]:
+            num_batches += 1
+            buckets[actual_bucket_id] = 0
+            if num_batches % log_freq == 0:
+                print(f"Iteration {i}, {num_batches} batches so far.")
+
+    num_batches += sum(1 for bucket in buckets if bucket != 0)
+    print(f"The dataset contains {i} data points and {num_batches} batches.")
 
 
 if __name__ == "__main__":
